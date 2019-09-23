96390afb359301f7fbcd2a1f1c4c2c601e659786
Loma <909622852@163.com>
2019-09-23 17:03:22 +0800

    单独改变包package-demo2

--- packages/package-demo2/index.js
+++ packages/package-demo2/index.js
@@ -1,12 +1,12 @@
 var package1 = require('package-demo1');
 function unit(a, b) {
-    console.log('unit:', a * b)
+    console.log('unit:', a * b);
     return a * b;
 }
 
 function useDemo1() {
     var sum = package1.add(2, 3);
-    console.log('sum:', sum)
+    console.log('sum:', sum);
 }
 
 useDemo1();
