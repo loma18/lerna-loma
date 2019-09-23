a4f9c074427f728198c3f4a3f6468349449fd851
Loma <909622852@163.com>
2019-09-23 18:30:33 +0800

    新增package-demo3,package.json文件需增加依赖引用,否则进到相应目录执行node index报错1

--- packages/package-demo2/index.js
+++ packages/package-demo2/index.js
@@ -1,11 +1,13 @@
 var package1 = require('package-demo1');
+var lodash = require('lodash');
 function unit(a, b) {
     console.log('unit:', a * b);
+    var c = lodash.join([a, b]);
+    console.log('c:', c);
     return a * b;
 }
-
 function useDemo1() {
-    var sum = package1.add(2, 3);
+    var sum = package1.add(2, 3, 4);
     console.log('sum:', sum);
 }
 
